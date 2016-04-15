64c74f5d4f57b16eead89536339231365925388f
smoylan1 <smoylan1@nd.edu>
2016-04-15 16:37:39 -0400

    Added files via upload

--- OpticalDiffraction/newconverter.C
+++ OpticalDiffraction/newconverter.C
@@ -2,6 +2,11 @@
 	string filename;
 	cout << "Please input the filename/path:\t";
 	cin >> filename;
+	
+	TCanvas *c1 = new TCanvas("c", "Fast Fourier Transform", 1600, 600);
+	c1->Divide(2,1);
+	c1->cd(1);
+	
 	int period_position = filename.find(".");
 	
 	string rawfile = filename.substr(0, period_position);
@@ -13,27 +18,42 @@
 	ifstream fIn(filename.c_str());
 	
 	if (!fIn){
-		cout << c_string << " could not be opened!" << endl;
+		cout << filename.c_str() << " could not be opened!" << endl;
 		exit(1);
 	}
 	
-	float time;
+	float time, pos;
 	int count;
 	
-	TH1F *h = new TH1F("h", "h", 8192, 0, 8191);
+	TH1F *h = new TH1F("h", "h", 151, 0, 150);
 	h->SetStats(kFALSE);
+	h->SetTitle(rawfile.c_str());
 	
-	tree->Branch("Time", &time, "chan/F");
+	h->GetXaxis()->SetTitle("Position (mm)");
+	h->GetXaxis()->CenterTitle();
+	
+	h->GetYaxis()->SetTitle("Intensity");
+	h->GetYaxis()->CenterTitle();
+	
+	tree->Branch("Position (mm)", &pos, "pos/F");
 	tree->Branch("Counts", &count, "count/I");
 	
 	while(fIn){
 		fIn >> time;
 		fIn >> count;
-		h->Fill(time, count);
+		pos = 0.25*time;
+		h->Fill(pos, count);
 		tree->Fill();
 	}
 	
 	h->Draw();
+	c1->cd(2);
 	
+	/*TH1 *hfft =0;
+	TVirtualFFT::SetTransform(0);
+	hfft = h->FFT(hfft, "MAG");
+	hfft->SetTitle("Fourier Transform");
+	hfft->Draw();
+	*/
 	outFile -> Write();
 }
